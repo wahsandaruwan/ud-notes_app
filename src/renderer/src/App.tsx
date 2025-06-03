d1040f8849d9a1cd23ae37715b4e2331a28d0736
wahsandaruwan <wahsandaruwan6@gmail.com>
2025-06-03 16:34:31 +0530

    Added updated project structure with all the dependencies

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div></div>
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <span className="text-4xl text-blue-500">Hello from Himal!</span>
+    </div>
+  )
 }
 
 export default App
